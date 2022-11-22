f3f69282d702ce1c254d60632d1d6d550c594563
Code 41 <washingtonbenton@gmail.com>
2022-11-22 12:30:41 -0500

    updating

---
+++ stackApp/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace stackApp;
+class Program
+{
+    static void Main(string[] args)
+    {
+//Create a trainer stack.
+        Stack<string> trainerStack = new Stack<string>();
+        trainerStack.Push("Mark");
+        trainerStack.Push("Jonathan");
+//Write the trainer stack.
+        Console.WriteLine("Your trainers are ");
+        foreach (var trainer in trainerStack) {
+            Console.Write(trainer + ", ");
+    
+        }
+//Create a batch stack.
+        Stack<string> batchStack = new Stack<string>();
+        batchStack.Push("Admir");
+        batchStack.Push("Benton");
+        batchStack.Push("Wengel");
+        batchStack.Push("Samuel");
+//Write the batch stack.
+        Console.WriteLine("");
+        Console.WriteLine("Group 1 of the batch includes ");
+        foreach (var batchMate in batchStack) {
+            Console.Write(batchMate + ", ");
+        }
+
+        
+        
+    }
+}
